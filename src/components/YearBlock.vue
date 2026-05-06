0e93038f21301ca2f25d2bc11e3f8bf634e30802
Jufebrown <jufebrown@gmail.com>
2026-05-06 03:48:37 -0500

    added YearBlock component

---
+++ src/components/YearBlock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="year">
+    <slot name="yearNumber"></slot>
+  </div>
+</template>
+
+<style scoped>
+.year {
+  margin-top: 2rem;
+  display: flex;
+  position: relative;
+}
+
+@media (min-width: 1024px) {
+  .year {
+    margin-top: 0;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  }
+}
+</style>
