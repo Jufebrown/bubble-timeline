329cb3ee65f7dfd278eeb6fa0c0fa370018b306b
Jufebrown <jufebrown@gmail.com>
2026-05-06 03:48:10 -0500

    added MonthBlock component

---
+++ src/components/MonthBlock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="month">
+    <slot name="monthNumber"></slot>
+  </div>
+</template>
+
+<style scoped>
+.month {
+  margin-top: 2rem;
+  display: flex;
+  position: relative;
+}
+
+@media (min-width: 1024px) {
+  .month {
+    margin-top: 0;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  }
+}
+</style>
