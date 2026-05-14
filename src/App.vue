e4a8a31298c563501b4797198e78020942fff69b
Jufebrown <jufebrown@gmail.com>
2026-05-14 04:22:56 -0500

    styled page title

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,21 @@ import TimeLine from './components/TimeLine.vue'
 <template>
   <header>
     <div class="wrapper">
-      <TimeLine msg="You did it!" />
+      <h1>Timeline of the AI Bubble</h1>
+      <TimeLine />
     </div>
   </header>
 </template>
 
 <style scoped>
+h1 {
+  margin: 0;
+  padding: 0 0 0.5em 1em;
+  color: #ddd;
+  font-size: 2em;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+
 @media (min-width: 1024px) {
 }
 </style>
