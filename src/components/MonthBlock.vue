1924ee0273b05e952965dc3271ee88631e6d104d
Jufebrown <jufebrown@gmail.com>
2026-05-20 02:11:47 -0500

    styling

--- src/components/MonthBlock.vue
+++ src/components/MonthBlock.vue
@@ -1,20 +1,100 @@
+<script setup lang="ts">
+import { useTimelineStore } from '@/stores/timeline'
+import TimelineItem from './TimelineItem.vue'
+import { ref } from 'vue'
+
+defineProps<{
+  month: {
+    short: string
+    long: string
+  }
+  monthIndex: number
+  yearIndex: number
+}>()
+
+const timelineStore = useTimelineStore()
+
+const extendMonth = ref(false)
+
+const getCorrectYearIndex = (index: number) => {
+  if (index > 6) {
+    return index % 14
+  } else {
+    return index
+  }
+}
+const getYearBackgroundColor = (index: number) => {
+  return timelineStore.backgroundColors[getCorrectYearIndex(index)]
+}
+const getMonthBackgroundColor = (yearIndex: number, monthIndex: number) => {
+  const correctYearIndex = getCorrectYearIndex(yearIndex)
+  if (timelineStore.monthBackgroundColors[correctYearIndex] !== undefined) {
+    return timelineStore.monthBackgroundColors[correctYearIndex][monthIndex]
+  } else {
+    return getYearBackgroundColor(correctYearIndex)
+  }
+}
+const determineShortOrLongMonthLabel = (
+  extendMonth: boolean,
+  month: { short: string; long: string },
+) => {
+  return extendMonth ? month.long : month.short
+}
+</script>
+
 <template>
-  <div class="month">
-    <slot name="monthNumber"></slot>
+  <div
+    class="month-block"
+    @click="extendMonth = !extendMonth"
+    :style="{
+      backgroundColor: getMonthBackgroundColor(yearIndex, monthIndex),
+      minWidth: extendMonth ? '7em' : '2em',
+    }"
+  >
+    <TimelineItem
+      v-for="(item, itemIndex) in timelineStore.getMonthItems(
+        timelineStore.years[yearIndex],
+        monthIndex,
+      )"
+      :key="itemIndex"
+      :itemData="item"
+      :extendMonth="extendMonth"
+      >{{ item }}</TimelineItem
+    >
+    <div
+      class="month-label"
+      :style="{
+        minWidth: extendMonth ? '6em' : '1.75em',
+      }"
+    >
+      {{ determineShortOrLongMonthLabel(extendMonth, month) }}
+    </div>
   </div>
 </template>
 
 <style scoped>
-.month {
-  margin-top: 2rem;
+.month-block {
+  height: 80vh;
   display: flex;
-  position: relative;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  padding: 0 1px 3px 1px;
+  margin: 0;
 }
 
-@media (min-width: 1024px) {
-  .month {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
+.month-label {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  text-align: center;
+  font-size: 1em;
+  color: #222222;
+  height: 1.75em;
+  border: 1px solid #222222;
+  padding: 0;
+  margin: 10px 0 0 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-self: stretch;
 }
 </style>
