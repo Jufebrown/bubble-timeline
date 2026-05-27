65198d5a676db1ec960d44508908e68d4088d442
Jufebrown <jufebrown@gmail.com>
2026-05-27 03:01:29 -0500

    made timeline controls component

---
+++ src/components/TimelineControls.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import ExpandButton from './ExpandButton.vue'
+import CollapseButton from './CollapseButton.vue'
+import FilterButton from './FilterButton.vue'
+import FilterMenu from './FilterMenu.vue'
+import { useTimelineStore } from '@/stores/timeline.ts'
+
+const timelineStore = useTimelineStore()
+</script>
+
+<template>
+  <menu>
+    <ExpandButton></ExpandButton>
+    <CollapseButton></CollapseButton>
+    <FilterButton></FilterButton>
+  </menu>
+  <FilterMenu v-if="timelineStore.showFilterMenu"></FilterMenu>
+</template>
+
+<style scoped>
+menu {
+  margin: auto 10% auto 0;
+  display: flex;
+  flex-direction: row;
+  height: 2.5em;
+}
+
+.control-button {
+  background-color: #eee;
+  border: 2px solid #eee;
+}
+
+.expand-button {
+  border-radius: 5px 0 0 5px;
+}
+
+.filter-button {
+  border-radius: 0 5px 5px 0;
+}
+</style>
