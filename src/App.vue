59ac52bd80cfde54ce4fb32bac4ba48a01a4acfb
Jufebrown <jufebrown@gmail.com>
2026-05-27 02:55:28 -0500

    changed position of header

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,18 @@ h1 {
   font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
 }
 
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 5.5em;
+  align-items: flex-end;
+}
+
 @media (min-width: 1024px) {
 }
 </style>
